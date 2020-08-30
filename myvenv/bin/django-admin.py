d3e053da0a9410e34dcffbc44e99caf48a9561f7
kitatora <mina3.ryu0728@gmail.com>
2020-08-30 22:16:29 +0900

    20200830

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/minami.ryu/Documents/Github/housing_costs_calculation/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
