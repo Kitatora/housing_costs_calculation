22aa3efcd92080a41f181169a26d5ce8ba2501e4
kitatora <mina3.ryu0728@gmail.com>
2020-09-05 01:37:57 +0900

    20200904

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class IndexView(TemplateView):
-    template_name = "app/index.html"
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "app/calculation.html"
+    login_url = '/accounts/login/'
