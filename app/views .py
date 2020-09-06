4b67320c7075a468d62aee99ff5a09d9257d70e8
kitatora <mina3.ryu0728@gmail.com>
2020-09-06 23:01:20 +0900

    20200906

--- app/views .py
+++ app/views .py
@@ -20,11 +20,11 @@ class TopView(View):
             MMF_info = form.cleaned_data['MMF']
             OMF_info = form.cleaned_data['OMF']
             loan_info = form.cleaned_data['loan']
-            interest_info = form.cleaned_data['interest']/100
-            down_info = form.cleaned_data['down']
+            # interest_info = form.cleaned_data['interest']/100
+            # down_info = form.cleaned_data['down']
             repayment_info = form.cleaned_data['repayment']
             m_repayment = repayment_info*12
-            m_loan = loand_info*12
+            m_loan = loan_info*12
 
         return render(request, 'app/calculation.html', {
             'form': form, #これがないと入力フォームのデータが計算のたびに消える
@@ -32,12 +32,11 @@ class TopView(View):
             'total_RRF' : RRF_info*(repayment_info*120), #修繕積立総額
             'total_MMF' : MMF_info*(repayment_info*120), #管理費総額
             'total_OMF' : OMF_info*(repayment_info*120), #その他総額
-            'total_interest' : (price_info*loan_info/12*(1+m_loan)**m_repayment/((1+m_loan)**m_repayment-1)*m_loan-price_info, #ローン利息
+            'total_interest' : (price_info*loan_info/12*(1+m_loan)**m_repayment/((1+m_loan)**m_repayment-1)*m_loan)-price_info, #ローン利息
             'vat' : price_info*0.1, #消費税
-            'Miscellaneous' : price_info*0.001 #
-            'Miscellaneous_if_loan' : price_info*0.0025 #ローン みずほ銀行ローンサイト諸経費より
-            'property_tax' : price_info*0.004*(repayment_info*120) #固定資産税
-            'g_total' : , #総額
+            'Miscellaneous' : price_info*0.001, #
+            'Miscellaneous_if_loan' : price_info*0.0025, #ローン みずほ銀行ローンサイト諸経費より
+            'property_tax' : price_info*0.004*(repayment_info*120), #固定資産税
         })
 
 class AbtView(TemplateView):
