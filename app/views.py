919969b393844d69ddfda8c83e4fb6abc9631675
kitatora <mina3.ryu0728@gmail.com>
2020-09-25 10:15:21 +0900

    20200925

--- app/views.py
+++ app/views.py
@@ -55,18 +55,20 @@ class TopView(View):
         m_term = term_info*12  # 返済回数(月数)
         m_interest = interest_info/12  # ローン利息(月利)
         price_info = price_info  # 物件価格
-        total_RRF = RRF_info*m_term  # 修繕積立金/年
-        total_MMF = MMF_info*m_term  # 管理費/年
-        total_OMF = OMF_info*m_term  # その他費用/年
+        total_RRF = RRF_info*m_term  # 修繕積立金
+        total_MMF = MMF_info*m_term  # 管理費
+        total_OMF = OMF_info*m_term  # その他費用
         total_interest = round(((m_interest*(1+m_interest)**m_term)/((1+m_interest)**m_term-1))*loan_info)*m_term-loan_info #ローン利息総額
         vat = round(price_info*0.1)  # 消費税10%
         miscellaneous = round(price_info*0.001)  # 火災保険や登記非（一般的に物件価格の0.1%）
         miscelleneous_if_loan = round(price_info*0.025) # ローン保険等（みずほ銀行ローン計算からテスト20回行った結果）
         total_property_tax = round(price_info*0.004)*term_info  # 所得税/年
-
-        # p_li = []
-        # for i in range(m_loan):
-        #     p_li.append()
+        grand_total = price_info+total_RRF+total_MMF+total_OMF+total_interest+vat+miscellaneous+miscelleneous_if_loan+total_property_tax
+        initial = down_info+vat+miscellaneous+miscelleneous_if_loan
+        variable = price_info+total_RRF+total_MMF+total_OMF+total_interest+total_property_tax
+        p_li = []
+        for i in range(term_info+1):
+            p_li.append(initial+(variable/term_info)*i)
 
         return render(request, 'app/calculation.html', {
             'form': form,
@@ -79,7 +81,7 @@ class TopView(View):
             'miscellaneous': miscellaneous,
             'miscellaneous_if_loan': miscelleneous_if_loan,  # ローン みずほ銀行ローンサイト諸経費より
             'property_tax': total_property_tax,  # 固定資産税
-            'grand_total': price_info+total_RRF+total_MMF+total_OMF+total_interest+vat+miscellaneous+miscelleneous_if_loan+total_property_tax,
+            'grand_total': grand_total, #総計
         })
 
 
